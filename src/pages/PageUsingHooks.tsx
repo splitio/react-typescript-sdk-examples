26f68cb3ab01ead13d9b8bd9dcfc48f25e918adf
Emiliano Sanchez <emiliano.sanchez@split.io>
2023-11-21 16:42:38 -0300

    Update example to use React SDK v1.10.1, with new hooks

--- src/pages/PageUsingHooks.tsx
+++ src/pages/PageUsingHooks.tsx
@@ -1,9 +1,8 @@
-import React, { useContext } from 'react';
-import { useClient, useTreatments, SplitContext } from '@splitsoftware/splitio-react';
+import React from 'react';
+import { useSplitClient, useSplitTreatments } from '@splitsoftware/splitio-react';
 import { feature_flag_1, feature_flag_2, feature_flag_3 } from '../sdkConfig';
-import { ISplitContextValues } from '@splitsoftware/splitio-react/types/types';
 
-/* This example shows useClient and useTreatments hooks */
+/* This example shows useSplitClient and useSplitTreatments hooks */
 
 function Loading() {
   return <div>Loading SDK...</div>
@@ -15,30 +14,26 @@ function Timedout() {
 
 export default function PageUsingHooks() {
 
-  /* `useContext(SplitContext)` lets you access the SDK status (`isReady`, `isTimedout`, `lastUpdate`) when using hooks,
-   * to conditionally render your components, for example, showing a Loading label until the SDK is ready. */
-  const { isReady, isTimedout }: ISplitContextValues = useContext(SplitContext);
-
-  /* `useTreatments` returns the evaluated treatments of the given list of feature flag names.
-   * While the SDK is not ready, treatments values are `control`. */
-  const treatment: SplitIO.TreatmentsWithConfig = useTreatments([feature_flag_1]);
+  /* `useSplitTreatments` returns the evaluated treatments of the given list of feature flag names along with the SDK status (`isReady`, `isReadyFromCache`, `isTimedout`, `lastUpdate`, etc).
+   * While the SDK is not ready or ready from cache, treatments values are `control`. */
+  const { treatments, isReady, isTimedout } = useSplitTreatments({ names: [feature_flag_1] });
 
   const FeatureOne = isReady ? (
     <div className="App-section">
       <h4>{`Feature flag: ${feature_flag_1}`}</h4>
-      <p>{`Treatment value: ${treatment[feature_flag_1].treatment}`}</p>
+      <p>{`Treatment value: ${treatments[feature_flag_1].treatment}`}</p>
     </div>
   ) : <Loading />;
 
-  /* `useClient` returns an SDK client with a given optional user key and traffic type.
-   * If key is not provided, it returns the client at SplitContext.
+  /* `useSplitClient` returns an SDK client with a given optional split key (e.g., user id) and traffic type.
+   * If `splitKey` is not provided, it returns the client at SplitContext.
    * If it is not inside the scope of a `SplitFactory` component, it returns `null`. */
-  const client: SplitIO.IClient | null = useClient('other_user');
-  const treatments: SplitIO.TreatmentsWithConfig = client ? client.getTreatmentsWithConfig([feature_flag_2, feature_flag_3]) : {};
+  const { client } = useSplitClient({ splitKey: 'other_user' });
+  const otherTreatments = client ? client.getTreatmentsWithConfig([feature_flag_2, feature_flag_3]) : {};
   const OtherFeatures = (
     isReady ? (
       <div className="App-section">{
-        Object.entries(treatments).map(([featureFlagName, treatment]) =>
+        Object.entries(otherTreatments).map(([featureFlagName, treatment]) =>
           <div key={featureFlagName} >
             <h4>{`Feature flag: ${featureFlagName}`}</h4>
             <p>{`Treatment value: ${treatment.treatment}`}</p>
